6ba10743b1858d8d814f3b1786d3566c035595a6
Chuankai Lin <chuankai@kai.idv.tw>
2014-09-28 21:27:52 +0800

    Add calibration page route

--- weightapp.rb
+++ weightapp.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 require 'erubis'
+require './calibration'
+
+calib = Calibration.instance
+
+begin
+f = File.open("/sys/bus/platform/drivers/hx711/raw", "r") 
+rescue Exception => e
+puts 'Cannot opnen hx711/raw'
+puts e
+end
+
+def read_raw
+	f.read
+end
 
 get '/' do 
 	erb :index 
@@ -10,5 +24,14 @@ get '/calibration' do
 end
 
 get '/calibration/:name' do |gram|
-	gram
+	if (gram == '0')
+		calib.clear_calibration_data
+	end
+	if calib.add_value_raw?(gram.to_i, read_raw.to_i)
+		erb :calibration_done
+	end
+end
+
+get '/calibration_done' do
+	erb :calibration_done
 end
