f43bc7fdacea9864cfce79c6822d65e79615593e
Chuankai Lin <chuankai@kai.idv.tw>
2014-10-05 11:19:03 +0800

    WeightLogger class implementation

--- weight_log_trig.rb
+++ weight_log_trig.rb
@@ -5,12 +5,24 @@ require 'calibration'
 require 'singleton'
 require 'date'
 
+module LoggerState
+	DISABLED = 1
+	ENABLED_STOPPED = 2
+	ENABLED_RUNNING = 3
+end
+
+class Time
+	def secs_of_today
+		(hour * 60 + min) * 60 + sec
+	end
+end
+
 class WeightLogger
 	include Singleton
 
 	def initialize
 		Dir.mkdir('log') unless Dir.exists?('log')
-		@enabled = false
+		@state = LoggerState.DISABLED
 	end
 
 	def config(frequency, duration, enable_mail_notification, mail_address) 
@@ -21,32 +33,45 @@ class WeightLogger
 	end
 
 	def start
-		if (!@enabled)
+		name = Date.today.to_s
+		begin
+			f= File.open(name, 'a');
+		rescue
+			puts 'File open failed'
+		end
+		if (@state == LoggerState.DISABLED)
 			EM.run do
 				@timer_stopped = false
 				EM.add_periodic_timer(@freq) do
-					if @timer_stopped
+					if @state == LoggerState.ENABLED_STOPPED
 						EM.stop_event_loop
-						@enabled = false
+						@state = LoggerState.DISABLED
 					end
 
-					name = Date.today.to_s + '.txt'
-					begin
-					File.open(name, "a") do |f|
-						gram = Calibratin.instance.value_from_raw(IO.read('/sys/bus/platform/drivers/hx711/raw'))
-						file.puts("#{time} #{gram}")
+					if name != Date.today.to_s
+						f.close
+						send_mail(name + '.txt') if @mail_notification
+						name = Date.today.to_s
+						begin
+						f = File.open(name + '.txt', 'a')
+						rescue
+						puts 'File opne failed'
+						end
 					end
-					rescue
-					puts 'File open failed'
-					end
+					gram = Calibratin.instance.value_from_raw(IO.read('/sys/bus/platform/drivers/hx711/raw'))
+					f.puts "#{Time.now.secs_of_today} #{gram}"
 				end
 			end
-			@enabled = true
+			@state = LoggerState.ENABLED_RUNNING
 		end
 	end
 
 	def stop
 		@timer_stopped = true
+	end
+
+	def send_mail
+		puts 'send_mail'
 	end
 end
 
