4879c3338c54ff3d78805398a8b21757d4129fd7
Chuankai Lin <chuankai@kai.idv.tw>
2016-07-11 16:27:46 +0800

    Clean up

--- weight_log.rb
+++ weight_log.rb
@@ -193,7 +193,7 @@ class WeightLogger
 					end
 
 					diff = queue.shift - gram
-                    # action(50)
+
 					if (diff > 10 && trend_count == 3)
 						trend_count = 0
 						action(diff)
